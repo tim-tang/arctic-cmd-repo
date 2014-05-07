d3c45e16c7346c9d19a74d6f9d2b9dc75cd4d793
tim.tang <tang.jilong@139.com>
2014-05-07 12:57:19 +0800

    [arctic-cmd-repo] - Add spec test for build.

--- backbone-subroute/spec.js
+++ backbone-subroute/spec.js
@@ -1,5 +1,6 @@
 define(function(require) {
     var module = require('./dist/backbone-subroute');
+    //var expect = require('expect');
     describe('backbone-subroute', function() {
         it('should has VERSION', function() {
             expect(module.VERSION).to.be.a('string');
