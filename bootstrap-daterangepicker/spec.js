de3eb1639f63759fc7785719d61065a3835b7389
tim.tang <tang.jilong@139.com>
2014-05-08 21:44:32 +0800

    [arctic-cmd-repo] - Tunning js test case.

--- bootstrap-daterangepicker/spec.js
+++ bootstrap-daterangepicker/spec.js
@@ -1,9 +1,10 @@
 define(function(require) {
+    var $ = require("$");
     var module = require('./dist/daterangepicker-debug');
     var expect = require('expect');
     describe('bootstrap-daterangepicker', function() {
         it('should has function', function() {
-            //expect(module.setEndDate).to.be.a('function');
+            expect($.fn.daterangepicker).to.be.a('function');
         });
     });
 });
