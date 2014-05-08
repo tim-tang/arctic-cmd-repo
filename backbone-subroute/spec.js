1367a05aaca255660d4f6cb0f1678314bd7851b2
tim.tang <tang.jilong@139.com>
2014-05-08 09:03:25 +0800

    [arctic-cmd-repo] - Cmdize bootstrap daterangepick plugin.

--- backbone-subroute/spec.js
+++ backbone-subroute/spec.js
@@ -1,6 +1,5 @@
 define(function(require) {
     var module = require('./dist/backbone-subroute');
-    //var expect = require('expect');
     describe('backbone-subroute', function() {
         it('should has VERSION', function() {
             expect(module.VERSION).to.be.a('string');
---
+++ bootstrap-daterangepicker/Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+    var pkg = grunt.file.readJSON('package.json');
+
+    grunt.initConfig({
+        pkg: pkg,
+
+        download: {
+            options: {
+                dest: 'src',
+            },
+            js: {
+                options: {
+                    transform: function(code) {
+                        return [
+                            'define(function(require, exports, module) {',
+                            "var jQuery = require('$');",
+                            "var moment = require('moment');",
+                            code.replace(/window\.jQuery/g, "jQuery"),
+                            code.replace(/window\.moment/g, "moment"),
+                            "});"
+                        ].join('\n');
+                    }
+                },
+                //note: can not be found by version.
+                url: 'https://raw.githubusercontent.com/dangrossman/bootstrap-daterangepicker/master/daterangepicker.js',
+                name: 'bootstrap.daterangepicker.js'
+            },
+            css: {
+                //options: {
+                //    transform: function(code) {
+                //        return code.replace(/\.\.\/fonts\//g, './fonts/');
+                //    }
+                //},
+                url: 'https://raw.githubusercontent.com/dangrossman/bootstrap-daterangepicker/master/daterangepicker-bs3.css',
+                name: "daterangepicker-bs3.css"
+            },
+        }
+    });
+
+    grunt.loadGlobalTasks('spm-build');
+    grunt.util._.merge(grunt.config.data, require('spm-build').config);
+
+    grunt.loadTasks('../_tasks/download/tasks');
+    grunt.registerTask('build', ['download', 'spm-build']);
+};
