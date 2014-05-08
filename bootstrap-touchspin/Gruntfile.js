df7f161dc49d7a5318d9d12bb1c4b5dadb6c0419
tim.tang <tang.jilong@139.com>
2014-05-08 12:11:22 +0800

    [arctic-cmd-repo] - cmdize bootstrap touchspin js.

---
+++ bootstrap-touchspin/Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+    var pkg = grunt.file.readJSON('package.json');
+
+    grunt.initConfig({
+        pkg: pkg,
+
+        download: {
+            options: {
+                dest: 'src',
+            },
+            js: {
+                options: {
+                    transform: function(code) {
+                        return [
+                            'define(function(require, exports, module) {',
+                            "var jQuery = require('$');",
+                            code
+                            "});"
+                        ].join('\n');
+                    }
+                },
+                url: 'https://raw.githubusercontent.com/istvan-ujjmeszaros/bootstrap-touchspin/<%= pkg.version %>/bootstrap-touchspin/bootstrap.touchspin.js',
+                name: 'bootstrap.touchspin.js'
+            }
+        }
+    });
+
+    grunt.loadGlobalTasks('spm-build');
+    grunt.util._.merge(grunt.config.data, require('spm-build').config);
+
+    grunt.loadTasks('../_tasks/download/tasks');
+    grunt.registerTask('build', ['download', 'spm-build']);
+};
